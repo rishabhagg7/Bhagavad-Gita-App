3c52dedb9f89cc8cc8832bd59d66be649816eb27
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-12-25 20:44:15 +0530

    updated app dependencies and added persistent state to cards

--- app/src/main/java/com/example/bhagavadgita/ui/screens/AllChaptersScreen.kt
+++ app/src/main/java/com/example/bhagavadgita/ui/screens/AllChaptersScreen.kt
@@ -25,7 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -108,7 +108,7 @@ fun ChapterCard(
     onChapterItemClicked: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var expanded by remember{
+    var expanded by rememberSaveable{
         mutableStateOf(false)
     }
     OutlinedCard(
